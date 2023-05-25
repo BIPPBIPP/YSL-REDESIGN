b7acc5fcab6630e5d7ddb75955416ad3a5e8d41b
BIPPBIPP <yeaji728@naver.com>
2023-05-25 18:10:34 +0900

    sub4

--- YSL/js/index.js
+++ YSL/js/index.js
@@ -1,4 +1,17 @@
 window.onload = function(){
-    let MenuState = false
-    let btnMenu
+    let dtList = document.querySelectorAll(".tabMenu>dt")
+    
+    for(i=0;i<dtList.length;i++){ 
+        dtList[i].addEventListener("click",function(){
+            for(j=0;j<dtList.length;j++){
+                dtList[j].classList.remove("on")
+            }
+           this.classList.add("on")
+           
+        //    this는 앞에서 선택한 dt를 가리키는 것.
+        // 클릭하는 맥락 안에서 클릭한 태그를 this로 표현할 수 있다.
+        
+    })
+}
+   
 }
