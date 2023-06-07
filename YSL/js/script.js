b37d1ec64404b3815bf5cd95f83e5e465505b482
BIPPBIPP <yeaji728@naver.com>
2023-06-07 18:10:37 +0900

    menu

--- YSL/js/script.js
+++ YSL/js/script.js
@@ -1,4 +1,24 @@
 window.onload = function(){
+
+// mainpage slide
+let MainSlide =  new Swiper('.station',{
+    loop:true,
+    direction:'horizontal',
+    pagination: {
+        el:".swiper-pagination",
+        type: "bullets",
+        clickable: true,
+    },
+    autoplay: {
+        delay: 10000,
+    },
+    speed:2000,
+
+})
+
+
+
+// subpage 3 
     let dtList1 = document.querySelectorAll(".tabMenu1>dt")
     
     for(i=0; i<dtList1.length; i++){ 
@@ -12,16 +32,6 @@ window.onload = function(){
         //    this는 앞에서 선택한 dt를 가리키는 것.
         // 클릭하는 맥락 안에서 클릭한 태그를 this로 표현할 수 있다.
         
-        let sub4Cont = document.querySelectorAll(".tabMenu2>dt")
-    
-        for(g=0; g<sub4Cont.length; g++){ 
-            sub4Cont[g].addEventListener("click",function(){
-                for(k=0;k<sub4Cont.length;k++){
-                    sub4Cont[k].classList.remove("on")
-                }
-               this.classList.add("on")
-            })
-        }
 
 
 }
