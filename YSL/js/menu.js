7f6871edb7359ec9907474a406617a7befec72c8
BIPPBIPP <yeaji728@naver.com>
2023-06-08 20:01:51 +0900

    sub2해야함

---
+++ YSL/js/menu.js
@@ -0,0 +1,76 @@
+$(document).ready(function(){
+    // btnMenu클래스 버튼을 클릭했을 때 nav태그가 보이게 되도록 기능
+    let menuState = false
+    // 변수는 바깥에 저장, 안그럼 함수 안에서 계속 false로 저장됨
+// 1. 메뉴 버튼 클릭 후, 메뉴창 오픈
+$(".btnMenu").click(function(e){
+
+ e.preventDefault()
+//  리턴되어 저장되는 함수를 막아줌
+
+    if(menuState==false){
+        // 메뉴가 현재 닫혀있는 상태 ->여는 기능
+        $(".btnMenu").addClass("close")
+        $("nav").addClass("on")
+        menuState = true
+    }else{
+        // 메뉴가 현재 열려 있는 상태 -> 닫는 기능
+        $(".btnMenu").removeClass("close")
+        $("nav").removeClass("on")
+        menuState = false
+    }
+    
+ 
+   
+   
+})
+
+// 2.1뎁스 클릭 -> 2뎁스 오픈 & close // => 열고 닫는 메뉴 완성
+$(".gnb>li").click(function(){
+
+
+if($(this).hasClass("on")==true){
+    $(this).css("height","50px")
+    $(this).removeClass("on")
+}else{
+    $(this).css("height","50px")
+    $(this).removeClass("on")
+    let gnb2dep = $(this).children("ul").children().length
+    $(this).css("height",(gnb2dep+1)*50+"px")
+    $(this).addClass("on")
+}
+
+// 2뎁스를 클릭했을 때 1뎁스가 닫히지 않게 된다.
+})
+$(".gnb ul").click(function(){
+    return false
+})
+
+
+
+
+// 3. lnb 메뉴 클릭 시, 작동
+$(".lnb>li").click(function(){
+    //1뎁스 li를 클릭했을 때, 2뎁스 목록이 보이게 하는 기능
+
+// 리스트 클릭했을 때 내가 클릭한 리스트한테 on클래스가 있다 / 없다
+
+if($(this).hasClass("on")==true){
+    $(this).css("height","50px")
+    $(this).removeClass("on")
+}else{
+    $(this).css("height","50px")
+    $(this).removeClass("on")
+
+    let lnb2dep = $(this).children("ul").children().length
+    $(this).css("height",(lnb2dep+1)*50+"px")
+    $(this).addClass("on")
+}
+// => 열고 닫는 메뉴 완성
+
+
+})
+$(".lnb ul").click(function(){
+    return false
+})
+})
--- YSL/js/script.js
+++ YSL/js/script.js
@@ -1,38 +1,64 @@
-window.onload = function(){
+$(document).ready(function(){
 
-// mainpage slide
-let MainSlide =  new Swiper('.station',{
-    loop:true,
-    direction:'horizontal',
-    pagination: {
-        el:".swiper-pagination",
-        type: "bullets",
-        clickable: true,
-    },
-    autoplay: {
-        delay: 10000,
-    },
-    speed:2000,
+    // mainpage slide
+    let MainSlide =  new Swiper('.station1',{
+        loop:true,
+        direction:'horizontal',
+        pagination: {
+            el:".swiper-pagination",
+            type: "bullets",
+            clickable: true,
+        },
+        autoplay: {
+            delay: 10000,
+        },
+        speed:2000,
+
+    });
+
+
+    // sub1-2 page
+
+    let swiper4 = new Swiper(".station4",{
+        slidesPerView: 3,
+        spaceBetween: 10,
+        centeredSlides: true,
+        pagination: {
+            el:".swiper-pagination",
+            type: "bullets",
+            clickable: true,
+        },
+      
+    });
+    let swiper5 = new Swiper(".station5",{
+        slidesPerView: 5,
+        spaceBetween: 5,
+        centeredSlides: true,
+        pagination: {
+            el:".swiper-pagination",
+            type: "bullets",
+            clickable: true,
+        },
+      
+    });
+    
+
+
+// subpage 3 
+
+$(".tabMenu1>dt").click(function(e){
+    e.preventDefault()
+    // 1.dt태그들이 on클래스가 지워져야 함
+    // 2.내가 클릭한 dt태그한테만 on클래스가 추가
+    $(".tabMenu1>dt").removeClass("on")
+    $(this).addClass("on")
+    // this는 내가 클릭한 dt태그를 지칭.
+})
+
 
 })
 
 
 
-// subpage 3 
-    let dtList1 = document.querySelectorAll(".tabMenu1>dt")
-    
-    for(i=0; i<dtList1.length; i++){ 
-        dtList1[i].addEventListener("click",function(){
-            for(j=0; j< dtList1.length; j++){
-                dtList1[j].classList.remove("on")
-            }
-           this.classList.add("on")
-        })
-    }     
-        //    this는 앞에서 선택한 dt를 가리키는 것.
-        // 클릭하는 맥락 안에서 클릭한 태그를 this로 표현할 수 있다.
+
         
-
-
-}
-   
